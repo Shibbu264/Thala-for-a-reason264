c952bcd347e8227549ec96d3f0c53c357d1ceafe
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2023-12-19 19:27:38 +0530

    Update mycontact.tsx

--- src/components/mycontact.tsx
+++ src/components/mycontact.tsx
@@ -23,7 +23,7 @@ return(
     <a href="https://twitter.com/shivuuuuu264" target="_blank">
     <img src='https://img.shields.io/badge/twitter-%2300acee.svg?&style=for-the-badge&logo=twitter&logoColor=white' alt='twitter' className="my-3 w-32" />
     </a>
-    <a href="https://linkedin.com/in/https://www.linkedin.com/in/shivanshu-ranjan-0671a3245/" target="_blank">
+    <a href="https://www.linkedin.com/in/shivanshu-ranjan-0671a3245/" target="_blank">
     <img src='https://img.shields.io/badge/linkedin-%231E77B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white' alt='linkedin'className="my-3 w-32" />
     </a>
     <a href="https://instagram.com/shivuuuuu__264" target="_blank">
