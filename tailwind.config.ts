481789f5a3f2a96865c0d407713fab984d7a5fe5
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2023-12-19 18:28:56 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,22 @@
 // tailwind.config.ts
+import type { Config } from 'tailwindcss'
 
-module.exports = {
+
+  const config: Config = {
     content: [
-      './src/**/*.{js,ts,jsx,tsx}',
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        },
+      },
     },
     plugins: [],
   }
-  
+  export default config
